fbc22197b27e91f60ebaa4cef592d750824f7f2e
acehb <156779044+acehb@users.noreply.github.com>
2026-01-14 23:58:08 +0800

    Add files via upload

---
+++ PiezoTrueEQL.m
@@ -0,0 +1,28 @@
+function q=PiezoTrueEQL(x_c,x_e,w_noise,b)
+%Inputs xc:control factor at which estimation of the true expected loss is concerned; x_e:quadrature nodes; w_noise:quadrature weights times xi(x_e)
+%b:upper bound of the loss function(omit for unbounded loss function).
+%Outputs q:true EQL
+
+Nx_e=size(x_e,1);
+Lent=100e-3+x_c(1)*100e-3;
+Heit=0.6e-3+x_c(2)*0.6e-3;
+d31t=1.5e-11+x_e(:,1)*1.5e-11;
+vt=90+x_e(:,2)*20;
+ydtemp=zeros(Nx_e,1);ymtemp=zeros(Nx_e,1);
+losstemps=zeros(Nx_e,1);losstemp=zeros(Nx_e,1);
+losstempm=zeros(Nx_e,1);
+if(nargin==3)
+    for k=1:Nx_e
+        [ydtemp(k),ymtemp(k)]=PiezoelectricActuator(Lent,Heit,d31t(k),vt(k),2);
+        [losstemp(k),losstemps(k),losstempm(k)]=lossPiezo(ydtemp(k),ymtemp(k));
+    end
+    loss_1=w_noise*losstemps;q=loss_1+losstempm(1);
+else
+    for k=1:Nx_e
+        [ydtemp(k),ymtemp(k)]=PiezoelectricActuator(Lent,Heit,d31t(k),vt(k),2);
+        [losstemp(k),losstemps(k),losstempm(k)]=lossPiezo(ydtemp(k),ymtemp(k),b);
+    end
+    q=w_noise*losstemp;
+end
+
+end
