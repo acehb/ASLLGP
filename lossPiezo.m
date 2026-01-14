83146a0abddf0ae89f4c6638b614b4cd8009893b
acehb <156779044+acehb@users.noreply.github.com>
2026-01-14 23:52:10 +0800

    Update lossPiezo.m

--- lossPiezo.m
+++ lossPiezo.m
@@ -1,9 +1,13 @@
-function [L,L_d,L_m]=lossPiezo(yd,ym)
-%Inputs yd:tip deflection of the beam;ym: mass of the beam
+function [L,L_d,L_m]=lossPiezo(yd,ym,b)
+%Inputs yd:tip deflection of the beam;ym: mass of the beam;%b:upper bound of the loss function(omit for unbounded loss function).
 %Outputs L:total quality cost; L_d:quality cost incurred by the tip deflection;L_m:quality cost incurred by the mass of the beam.
 tm=7.5e-5;td=-8e-5;
 k_1=3e3;k_2=1;
 L_m=k_2*(ym-tm).*(1-((ym-tm)<0));
 L_d=k_1*(yd-td).^2; 
-L=L_d+L_m;
+if(nargin==2)
+        L=L_d+L_m;
+else
+    L0=L_d+L_m;L=b*(1-exp(-L0/b));
 end
+
