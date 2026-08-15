6394c740a37eecefe732b766e4c5048e208427d9
acehb <156779044+acehb@users.noreply.github.com>
2026-08-15 19:30:54 +0800

    Update gpfitSLL.m

--- gpfitSLL.m
+++ gpfitSLL.m
@@ -1,7 +1,7 @@
 function [theta,fval,beta,tao,irRes,irx,RIf,ifRIf,transpar]=gpfitSLL(xin,yin,ubound)
 %Inputs xin:the design used in the experiment;yin:outputs data from the experiment;ubound:upper bound of the loss function(omit for unbounded loss function).
 %Outputs theta:maximum integrated likelihood estimate of correlation parameters;fval:-2*(log integrated likelihood);beta:maximum likelihood estimate of the regression coefficients;%tao:maximum integrated likelihood estimate of variance parameter;
-%irRes,irx,RIf,ifRIf are several vectors and matrices to be used by gppredict to compute point predictions and prediction intervals %transpar:maximum integrated likelihood estimate of a and epsilon in (4.1);%SLLloomrae,SLLloomrlengthci,SLLloomcover,SLLloo95rae,SLLloo95rlengthci:performance measures basesd on leave-one-out cross-validation
+%irRes,irx,RIf,ifRIf are several vectors and matrices to be used by gppredict to compute point predictions and prediction intervals;transpar:maximum integrated likelihood estimate of a and epsilon
 format long g
 %global z x lx Sign
 
